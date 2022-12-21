cda87bf2c16b99fdf705ec7fec4af33ac3dc1130
Jader Neira <jader.neira@gmail.com>
2022-12-20 21:09:05 -0500

    add pokeApi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,47 @@
-import { Button } from '@nextui-org/react'
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 
+import { pokeApi } from '../api'
 import { Layout } from '../components/layouts'
+import { PokemonListResponse, SmallPokemon } from '../interfaces'
 
 
-const HomePage: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[]
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  
   return (
     <Layout title='Pokemons List'>
-      <Button color={'gradient'}> Hello World </Button>
+      <ul>
+        {
+          pokemons.map( ({id, name}) => (
+            <li key={ id }>
+              #{ id } - { name }
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon/?limit=151')
+
+  const pokemons: SmallPokemon[] = data.results.map( (pokemon, idx) => ({
+      ...pokemon,
+      id: idx + 1,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ idx + 1 }.svg`    
+  }))
+    
+  return {
+    props: {
+      pokemons
+    }
+  }
 }
 
 export default HomePage
