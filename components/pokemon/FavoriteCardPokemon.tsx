2dc220cf386811de5cc6a533d3ef32fe001951a8
Jader Neira <jader.neira@gmail.com>
2022-12-21 19:59:17 -0500

    add FavoriteCardPokemon

---
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -0,0 +1,19 @@
+import { Card, Grid } from "@nextui-org/react"
+
+interface Props {
+  pokemonId: number
+}
+
+export const FavoriteCardPokemon: React.FC<Props> = ({ pokemonId }: Props) => {
+  return (
+    <Grid xs={6} sm={3} xl={1} key={pokemonId}>
+      <Card isHoverable isPressable css={{ padding: 10 }}>
+        <Card.Image 
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
+          width={'100%'}
+          height={ 140 }
+        />
+      </Card>
+    </Grid>
+  )
+}
