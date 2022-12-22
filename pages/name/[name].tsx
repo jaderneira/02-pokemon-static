f24800170f21a21d44d1e08c404ff728676592e8
Jader Neira <jader.neira@gmail.com>
2022-12-22 17:05:40 -0500

    config  for ISG

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -122,17 +122,29 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
     paths: pokemonNames.map( name => ({
       params: { name }
     })),
-    fallback: false,
+    fallback: 'blocking',
   }
 };
  
 export const getStaticProps: GetStaticProps = async ({ params, }) => {
   const { name } = params as { name: string };
+
+  const pokemon = await getPokemonInfo(name.toLowerCase());
+
+  if(!pokemon){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
   
   return {
     props: {
-      pokemon: await getPokemonInfo(name)
+      pokemon
     },
+    revalidate: 86400,
   };
 };
  
