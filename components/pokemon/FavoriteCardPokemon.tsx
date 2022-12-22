e0c32b8b173888259a7158bad868ee281f5188b7
Jader Neira <jader.neira@gmail.com>
2022-12-21 20:06:06 -0500

    add onFavoriteClicked

--- components/pokemon/FavoriteCardPokemon.tsx
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router"
 import { Card, Grid } from "@nextui-org/react"
 
 interface Props {
@@ -5,9 +6,15 @@ interface Props {
 }
 
 export const FavoriteCardPokemon: React.FC<Props> = ({ pokemonId }: Props) => {
+  const router = useRouter();
+
+  const onFavoriteClicked = () => {
+    router.push(`/pokemon/${ pokemonId }`)
+  }
+
   return (
     <Grid xs={6} sm={3} xl={1} key={pokemonId}>
-      <Card isHoverable isPressable css={{ padding: 10 }}>
+      <Card isHoverable isPressable css={{ padding: 10 }} onPress={ onFavoriteClicked }>
         <Card.Image 
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
           width={'100%'}
